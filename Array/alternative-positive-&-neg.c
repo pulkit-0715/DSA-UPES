775cb0424d7c59e6858e5568f92e36626b16aea4
pulkit-0715 <singhpulkit1506@gmail.com>
2026-02-07 09:01:33 +0530

    Question 3

--- Array/alternative-positive-&-neg.c
+++ Array/alternative-positive-&-neg.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Function to right rotate the array from 'start' to 'end'
 void rightRotate(int arr[], int start, int end) {
     int temp = arr[end];
     for (int i = end; i > start; i--) {
@@ -10,40 +9,40 @@ void rightRotate(int arr[], int start, int end) {
 }
 
 void rearrange(int arr[], int n) {
-    int i, j;
+    int wrongIndex = -1;
 
-    // Step 1: Partition negatives to left side
-    j = 0;
-    for (i = 0; i < n; i++) {
-        if (arr[i] < 0) {
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-            j++;
+    for (int i = 0; i < n; i++) {
+        
+        if (wrongIndex >= 0) {
+            if ((arr[i] >= 0 && arr[wrongIndex] < 0) ||
+                (arr[i] < 0 && arr[wrongIndex] >= 0)) {
+                
+                rightRotate(arr, wrongIndex, i);
+
+                if (i - wrongIndex >= 2)
+                    wrongIndex += 2;
+                else
+                    wrongIndex = -1;
+            }
         }
-    }
 
-    // Step 2: Rearrange alternately using rotation
-    int pos = j;   // index of first positive
-    int neg = 0;   // index of first negative
-
-    while (neg < pos && pos < n && arr[neg] < 0) {
-        rightRotate(arr, neg + 1, pos);
-        neg += 2;
-        pos++;
+        if (wrongIndex == -1) {
+            if (((arr[i] >= 0) && (i % 2 == 0)) ||
+                ((arr[i] < 0) && (i % 2 == 1))) {
+                wrongIndex = i;
+            }
+        }
     }
 }
 
 int main() {
-    int arr[] = {1, 2, -3, -4, -5, 6, -7, 8};
+    int arr[] = {-7,10,-6,2,3,-8,0,7,11,-20};
     int n = sizeof(arr) / sizeof(arr[0]);
 
     rearrange(arr, n);
 
-    printf("Rearranged array:\n");
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
-    }
 
     return 0;
 }
