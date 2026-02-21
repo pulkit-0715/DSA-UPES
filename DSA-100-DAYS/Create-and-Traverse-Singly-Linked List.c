4b515d72a31e24b7b6e5b5c0449a74e2b8223947
pulkit-0715 <singhpulkit1506@gmail.com>
2026-02-21 23:39:01 +0530

    Day 21 QUestion 1

---
+++ DSA-100-DAYS/Create-and-Traverse-Singly-Linked List.c
@@ -0,0 +1,66 @@
+/*
+Problem: Create and Traverse Singly Linked List
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the result
+
+Example:
+Input:
+5
+10 20 30 40 50
+
+Output:
+10 20 30 40 50
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if(n <= 0) return 0;
+
+    struct Node* head = NULL;
+    struct Node* temp = NULL;
+
+    for(int i = 0; i < n; i++) {
+        int value;
+        scanf("%d", &value);
+
+        struct Node* newNode = createNode(value);
+
+        if(head == NULL) {
+            head = newNode;
+            temp = head;
+        } else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    temp = head;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    return 0;
+}
