998c2d681cb011cdf42a8c1e9247253741c3dc46
pulkit-0715 <singhpulkit1506@gmail.com>
2026-02-28 23:15:44 +0530

    Day 28 Q1

---
+++ DSA-100-DAYS/Circular-Linked-List-Creation-and-Traversal.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void append(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+
+    if (*head == NULL) {
+        *head = newNode;
+        newNode->next = *head;
+        return;
+    }
+
+    struct Node* temp = *head;
+    while (temp->next != *head)
+        temp = temp->next;
+
+    temp->next = newNode;
+    newNode->next = *head;
+}
+
+void printCircular(struct Node* head) {
+    if (head == NULL)
+        return;
+
+    struct Node* temp = head;
+
+    do {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    } while (temp != head);
+}
+
+int main() {
+    int n, x;
+    struct Node* head = NULL;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &x);
+        append(&head, x);
+    }
+
+    printCircular(head);
+
+    return 0;
+}
