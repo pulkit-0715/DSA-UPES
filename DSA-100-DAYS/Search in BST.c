e3b21b866c4e5565b9fcdcfce0551f5d5982d24a
pulkit-0715 <singhpulkit1506@gmail.com>
2026-04-11 23:09:35 +0530

    Day 50 q2

---
+++ DSA-100-DAYS/Search in BST.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Tree Node structure
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+// Create new node
+struct TreeNode* createNode(int val) {
+    struct TreeNode* node = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+    node->val = val;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// Insert into BST (to build tree)
+struct TreeNode* insert(struct TreeNode* root, int val) {
+    if (root == NULL)
+        return createNode(val);
+
+    if (val < root->val)
+        root->left = insert(root->left, val);
+    else
+        root->right = insert(root->right, val);
+
+    return root;
+}
+
+// Search in BST (LeetCode 700 logic)
+struct TreeNode* searchBST(struct TreeNode* root, int val) {
+    if (root == NULL || root->val == val)
+        return root;
+
+    if (val < root->val)
+        return searchBST(root->left, val);
+    else
+        return searchBST(root->right, val);
+}
+
+// Inorder traversal (for display)
+void inorder(struct TreeNode* root) {
+    if (root != NULL) {
+        inorder(root->left);
+        printf("%d ", root->val);
+        inorder(root->right);
+    }
+}
+
+int main() {
+    struct TreeNode* root = NULL;
+    int n, val, key;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+
+    printf("Enter BST elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &val);
+        root = insert(root, val);
+    }
+
+    printf("Enter value to search: ");
+    scanf("%d", &key);
+
+    struct TreeNode* result = searchBST(root, key);
+
+    if (result != NULL) {
+        printf("Value Found! Subtree Inorder: ");
+        inorder(result);   // prints subtree like LeetCode output
+    } else {
+        printf("Value Not Found");
+    }
+
+    return 0;
+}
