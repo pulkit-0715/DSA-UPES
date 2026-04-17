fccde3ef89b8997c14b3cc94d203e03275e02545
pulkit-0715 <singhpulkit1506@gmail.com>
2026-04-17 23:35:16 +0530

    Day 45 Q1

---
+++ DSA-100-DAYS/Height of a Binary Tree.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    node->data = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node** queue = (struct Node**)malloc(n * sizeof(struct Node*));
+    int front = 0, rear = 0;
+
+    struct Node* root = createNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        if (i < n && arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    free(queue);
+    return root;
+}
+
+int height(struct Node* root) {
+    if (root == NULL)
+        return 0;
+
+    int left = height(root->left);
+    int right = height(root->right);
+
+    return (left > right ? left : right) + 1;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    printf("%d\n", height(root));
+
+    return 0;
+}
