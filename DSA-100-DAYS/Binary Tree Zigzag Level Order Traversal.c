5e27865eaf543032a798c0c519892f9a65df413d
pulkit-0715 <singhpulkit1506@gmail.com>
2026-04-19 23:28:41 +0530

    Day 54 q2

---
+++ DSA-100-DAYS/Binary Tree Zigzag Level Order Traversal.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 1000
+
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+struct Node* createNode(int val) {
+    struct Node* n = (struct Node*)malloc(sizeof(struct Node));
+    n->data = val;
+    n->left = n->right = NULL;
+    return n;
+}
+
+struct Node* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1)
+        return NULL;
+
+    struct Node* root = createNode(arr[0]);
+    struct Node* queue[MAX];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+    int i = 1;
+
+    while (i < n) {
+        struct Node* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = createNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = createNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void zigzag(struct Node* root) {
+    if (!root) return;
+
+    struct Node* queue[MAX];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    int leftToRight = 1;
+
+    while (front < rear) {
+        int size = rear - front;
+        int temp[MAX];
+
+        for (int i = 0; i < size; i++) {
+            struct Node* curr = queue[front++];
+
+            int index = leftToRight ? i : size - 1 - i;
+            temp[index] = curr->data;
+
+            if (curr->left)
+                queue[rear++] = curr->left;
+            if (curr->right)
+                queue[rear++] = curr->right;
+        }
+
+        for (int i = 0; i < size; i++)
+            printf("%d ", temp[i]);
+
+        leftToRight = !leftToRight;
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[MAX];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    struct Node* root = buildTree(arr, n);
+
+    zigzag(root);
+
+    return 0;
+}
